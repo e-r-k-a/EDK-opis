ed08a118b89cf75d9779e5f90233345f532a558a
Romek Kopias <romek68@gmail.com>
2018-03-05 15:07:56 +0100

    dodany test konstruktora

--- EDK_fx/src/Tests/pointTest.java
+++ EDK_fx/src/Tests/pointTest.java
@@ -2,13 +2,60 @@ package Tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
-class pointTest {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.time.format.*;
 
+
+
+import model.Point;
+
+
+public class pointTest {
+
+	private Point p;
+	private final int MAXDURATIONNANO = 50000000; //50ms na czas miêdzy utworzeniem obiektu a zrobieniem testu
+	
+	@BeforeEach
+	public void setUp() {
+		p = new Point(19.56, 38.67, "nazwa1", "opis1");
+
+	}
+	
 	@Test
-	void test1() {
-		assertEquals(1,1);
+	public void test1() {//testy konstruktora
+		assertEquals(19.56, p.getxGpsCoord());
+		assertEquals(38.67, p.getyGpsCoord());
+		assertEquals("nazwa1", p.getName());
+		assertEquals("opis1", p.getDescription());
+		
+		assertEquals(LocalDate.now(), p.getLocalDate());//sprawdzenie czy konstr wpisuje poprawnie date
+				
+		Duration duration=Duration.between(p.getLocalTime(), LocalTime.now());
+		if(duration.getNano() > MAXDURATIONNANO) {
+			fail("Zle wpisany czas do obiektu"); 
+		}
+		
+		
+		
+		
+		
+		
 	}
 
+	@Test
+	public void test2() {//testy konstruktora
+		assertEquals(19.56, p.getxGpsCoord());
+		assertEquals(38.67, p.getyGpsCoord());
+		assertEquals("nazwa1", p.getName());
+		assertEquals("opis1", p.getDescription());
+	}
 }
+
+
